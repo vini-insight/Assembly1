e44615f86625da7c6ab44ef9db42d433454c6787
Vinicius Vieira <wynny.eu@gmail.com>
2023-04-17 23:33:56 -0300

    lcd
    lcd

--- lcd.s
+++ lcd.s
@@ -1,160 +1,161 @@
-.macro start_lcd
+.macro enableLCD
+        GPIOpinLOW ENABLE
+        sleep_by zero_second mili_1_seconds
+        GPIOpinHIGH ENABLE
+        sleep_by zero_second mili_1_seconds
+        GPIOpinLOW ENABLE
+.endm
 
-        gpio_pin_out RS
-        gpio_pin_out ENABLE
-        gpio_pin_out DB7
-        gpio_pin_out DB6
-        gpio_pin_out DB6
-        gpio_pin_out DB5
-        gpio_pin_out DB4
+.macro clearLCD
 
-        gpio_pin_low RS
+        GPIOpinLOW RS
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_high DB5
-        gpio_pin_high DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinLOW DB5
+        GPIOpinLOW DB4
+        enableLCD
+
+        GPIOpinHIGH DB4
+        enableLCD
+.endm
+
+.macro startLCD
+
+        setGPIOoutputPin RS
+        setGPIOoutputPin ENABLE
+        setGPIOoutputPin DB7
+        setGPIOoutputPin DB6
+        setGPIOoutputPin DB6
+        setGPIOoutputPin DB5
+        setGPIOoutputPin DB4
+
+        GPIOpinLOW RS
+
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinHIGH DB5
+        GPIOpinHIGH DB4
+        enableLCD
         sleep_by zero_second, mili_5_seconds
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_high DB5
-        gpio_pin_high DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinHIGH DB5
+        GPIOpinHIGH DB4
+        enableLCD
         sleep_by zero_second, micro_150_seconds
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_high DB5
-        gpio_pin_high DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinHIGH DB5
+        GPIOpinHIGH DB4
+        enableLCD
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_high DB5
-        gpio_pin_low DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinHIGH DB5
+        GPIOpinLOW DB4
+        enableLCD
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_high DB5
-        gpio_pin_low DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinHIGH DB5
+        GPIOpinLOW DB4
+        enableLCD
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_low DB5
-        gpio_pin_low DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinLOW DB5
+        GPIOpinLOW DB4
+        enableLCD
 
-        enable_lcd
-        gpio_pin_high DB7
-        gpio_pin_low DB6
-        gpio_pin_low DB5
-        gpio_pin_low DB4
-        enable_lcd
+        enableLCD
+        GPIOpinHIGH DB7
+        GPIOpinLOW DB6
+        GPIOpinLOW DB5
+        GPIOpinLOW DB4
+        enableLCD
 
-        gpio_pin_low DB7
-        enable_lcd
+        GPIOpinLOW DB7
+        enableLCD
 
-        gpio_pin_high DB4
-        enable_lcd
+        GPIOpinHIGH DB4
+        enableLCD
 
-        gpio_pin_low DB4
-        enable_lcd
+        GPIOpinLOW DB4
+        enableLCD
 
-        gpio_pin_low DB7
-        gpio_pin_high DB6
-        gpio_pin_high DB5
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinHIGH DB6
+        GPIOpinHIGH DB5
+        enableLCD
 
-        gpio_pin_low DB6
-        gpio_pin_low DB5
-        enable_lcd
+        GPIOpinLOW DB6
+        GPIOpinLOW DB5
+        enableLCD
 
-        gpio_pin_high DB7
-        gpio_pin_high DB6
-        gpio_pin_high DB5
-        enable_lcd
+        GPIOpinHIGH DB7
+        GPIOpinHIGH DB6
+        GPIOpinHIGH DB5
+        enableLCD
 
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_low DB5
-        gpio_pin_low DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinLOW DB5
+        GPIOpinLOW DB4
+        enableLCD
 
-        gpio_pin_low DB7
-        gpio_pin_high DB6
-        gpio_pin_high DB5
-        gpio_pin_low DB4
-        enable_lcd
+        GPIOpinLOW DB7
+        GPIOpinHIGH DB6
+        GPIOpinHIGH DB5
+        GPIOpinLOW DB4
+        enableLCD
         .ltorg
 .endm
 
-.macro prefix_number_lcd
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_high DB5
-        gpio_pin_high DB4
-        enable_lcd
+.macro prefixNumberLCD
+        GPIOpinLOW DB7
+        GPIOpinLOW DB6
+        GPIOpinHIGH DB5
+        GPIOpinHIGH DB4
+        enableLCD
 .endm
 
-.macro write_lcd number
-        gpio_pin_high RS
-        prefix_number_lcd
+.macro writeLCD number
+        GPIOpinHIGH RS
+        prefixNumberLCD
 
-        @ D4
+        @ para D4
         MOV R1, #0b00001
-        AND R1, \number          @0001 & 0011 -> 0001
+        AND R1, \number         @ 0001 & 0011 -> 0001
         LDR R0, =DB4
         LDR R7, [R0, #8]
-        BL gpio_pin_turn
+        BL switchLogicValuePin
 
-        @ D5
+        @ para D5
         MOV R1, #0b00010
-        AND R1, \number          @ 0010 & 0011 -> 0010
+        AND R1, \number         @ 0010 & 0011 -> 0010
         LSR R1, #1              @ Desloca o bit 1x para direita  -> 0001
         LDR R0, =DB5
         LDR R7, [R0, #8]
-        BL gpio_pin_turn
+        BL switchLogicValuePin
 
-        @ D6
+        @ para D6
         MOV R1, #0b00100
-        AND R1, \number          @ 0100 & 0101 -> 0100
+        AND R1, \number         @ 0100 & 0101 -> 0100
         LSR R1, #2              @ Desloca o bit 2x para direita  -> 0001
         LDR R0, =DB6
         LDR R7, [R0, #8]
-        BL gpio_pin_turn
+        BL switchLogicValuePin
 
-        @ D7
+        @ para D7
         MOV R1, #0b01000
-        AND R1, \number          @ 01000 & 01000 -> 01000
+        AND R1, \number         @ 01000 & 01000 -> 01000
         LSR R1, #3              @ Desloca o bit 3x para direita  -> 00001
         LDR R0, =DB7
         LDR R7, [R0, #8]
-        BL gpio_pin_turn
-        enable_lcd
+        BL switchLogicValuePin
+        enableLCD
 .endm
-
-.macro clear_lcd
-
-        gpio_pin_low RS
-
-        gpio_pin_low DB7
-        gpio_pin_low DB6
-        gpio_pin_low DB5
-        gpio_pin_low DB4
-        enable_lcd
-
-        gpio_pin_high DB4
-        enable_lcd
-.endm
-
-.macro enable_lcd
-        gpio_pin_low ENABLE
-        sleep_by zero_second mili_1_seconds
-        gpio_pin_high ENABLE
-        sleep_by zero_second mili_1_seconds
-        gpio_pin_low ENABLE
-.endm
+@ fim do arquivo
