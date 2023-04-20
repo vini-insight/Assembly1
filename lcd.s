d98f19dd15ac126cb9153015e75216432e8deb05
Vinicius Vieira <wynny.eu@gmail.com>
2023-04-19 23:10:47 -0300

    lcd
    lcd

--- lcd.s
+++ lcd.s
@@ -1,5 +1,7 @@
 
-.macro habilitarLCD
+@ este arquivo utiliza as macros previamente definidas no arquivo "util.s". os comentários dessas macros já está lá
+
+.macro habilitarLCD                             @ executa os passos para habilitar LCD
         GPIOpinLOW PA18toENABLE
         sleep zeroSecond oneMILLIsecond
         GPIOpinHIGH PA18toENABLE
@@ -7,7 +9,7 @@
         GPIOpinLOW PA18toENABLE
 .endm
 
-.macro clearLCD
+.macro clearLCD                                 @ executa os passos para limpar LCD
 
         GPIOpinLOW PA2toRS
 
@@ -21,7 +23,7 @@
         habilitarLCD
 .endm
 
-.macro startLCD
+.macro startLCD                         @ inicializa o display 16x2 LCD HD44780 de acordo com os passos informandos em sua Ficha de Dados (datasheet)
 
         setGPIOoutputPin PA2toRS
         setGPIOoutputPin PA18toENABLE
@@ -123,37 +125,33 @@
         habilitarLCD
 .endm
 
-.macro writeLCD number
+.macro writeLCD number          @ executa os passos para escrever no LCD
         GPIOpinHIGH PA2toRS
         prefixNumberLCD
-
-        @ para D4
-        MOV R1, #0b00001
+        
+        MOV R1, #0b00001        @ para DB4
         AND R1, \number         @ 0001 & 0011 -> 0001
         LDR R0, =PG8toDB4
         LDR R7, [R0, #8]
         BL switchLogicValuePin
 
-        @ para D5
-        MOV R1, #0b00010
+        MOV R1, #0b00010        @ para DB5
         AND R1, \number         @ 0010 & 0011 -> 0010
-        LSR R1, #1              @ Desloca o bit 1x para direita  -> 0001
+        LSR R1, #1              @ Desloca o bit uma vez para direita  -> 0001
         LDR R0, =PG9toDB5
         LDR R7, [R0, #8]
         BL switchLogicValuePin
-
-        @ para D6
-        MOV R1, #0b00100
+        
+        MOV R1, #0b00100        @ para DB6
         AND R1, \number         @ 0100 & 0101 -> 0100
-        LSR R1, #2              @ Desloca o bit 2x para direita  -> 0001
+        LSR R1, #2              @ Desloca o bit duas vezes para direita  -> 0001
         LDR R0, =PG6toDB6
         LDR R7, [R0, #8]
         BL switchLogicValuePin
-
-        @ para D7
-        MOV R1, #0b01000
+        
+        MOV R1, #0b01000        @ para DB7
         AND R1, \number         @ 01000 & 01000 -> 01000
-        LSR R1, #3              @ Desloca o bit 3x para direita  -> 00001
+        LSR R1, #3              @ Desloca o bit três vezes para direita  -> 00001
         LDR R0, =PG7toDB7
         LDR R7, [R0, #8]
         BL switchLogicValuePin
